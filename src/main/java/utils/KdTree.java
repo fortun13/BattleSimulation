dda9edc6c505054451d20b2b6b63a708c9617a5e
Marek Wawrzos <marek.28.93@gmail.com>
2014-11-20 18:37:44 +0100

    Added descriptions

--- src/main/java/utils/KdTree.java
+++ src/main/java/utils/KdTree.java
@@ -7,16 +7,29 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ */
+
+/**
  * Created by Marek on 2014-11-15.
- *
  * That container gives possibility to fetch elements from given area in O(sqrt(n) + k),
  * where n is an count of elements and k is a count of results
+ *
+ * @param <T> type of tree elements
+ * @param <A> type of figure witch determines inclusion of elements in given area
  */
 public class KdTree<T, A> {
     private final Contains<T, A> contains;
     private Tree<T, A> tree;
     List<Comparator<T>> c;
 
+    /**
+     * Constructor fo KdTree
+     *
+     * @param content  list of points witch should be placed in tree
+     * @param comp     list of comparators for each dimension
+     * @param contains if determine inclusion of points and areas
+     * @throws KdTreeException thrown, when content contains non unique
+     */
     public KdTree(List<T> content, List<Comparator<T>> comp, Contains<T, A> contains) throws KdTreeException {
         c = comp;
         this.contains = contains;
@@ -33,10 +46,30 @@ public class KdTree<T, A> {
         }
     }
 
+    /**
+     * Inserts an point to the tree
+     *
+     * @param point element to insert
+     */
     public void addPoint(T point) {
         tree.addPoint(point);
     }
-    public void rmPoint(T point) { tree.rmPoint(point); }
+
+    /**
+     * Removes element from tree if such exists
+     *
+     * @param point point in same position will be removed
+     */
+    public void rmPoint(T point) {
+        tree.rmPoint(point);
+    }
+
+    /**
+     * Returns list of elements in give area
+     *
+     * @param area search area
+     * @return list of found elements
+     */
     public List<T> fetchElements(A area) {
         List<T> ret = new ArrayList<>();
         points = new ArrayList<>();
@@ -45,6 +78,13 @@ public class KdTree<T, A> {
         return ret;
     }
 
+    /**
+     * Returns at most k elements nearest to queryPoint from the set
+     *
+     * @param k          upper bound of found elements count
+     * @param queryPoint referred point
+     * @return list of found points
+     */
     public List<T> kNearestNeighbours(int k, T queryPoint) {
         List<T> res = new ArrayList<T>() {
             @Override
@@ -59,28 +99,99 @@ public class KdTree<T, A> {
     }
 
     public static class KdTreeException extends Throwable {
-
         public KdTreeException() {
             super("Two points with same coordinates occurred");
         }
     }
-    public interface Placed {
-        Point2D pos();
-    }
+
+    /**
+     * Classes witch that interface describes, rules of points and areas intersection in given hyperspace
+     *
+     * @param <T> type f point in given space
+     * @param <A> type of areas in given space
+     */
     public interface Contains<T, A> {
+        /**
+         * describes if area contains an point
+         *
+         * @param area  area of search
+         * @param point considered point
+         * @return result of comparison
+         */
         boolean contains(A area, T point);
+
+        /**
+         * describes if area contains hypercube described with list of points
+         *
+         * @param area   area of search
+         * @param points that point are intersect by hyperplane in following dimension
+         * @return result of comparison
+         */
         boolean contains(A area, ArrayList<T> points);
+
+        /**
+         * describes if area intersects hypercube described with list of points
+         *
+         * @param area      area of search
+         * @param points    that point are intersect by hyperplane in following dimension
+         * @param ascending if true half-hyperspace with coordinates in following dimension greater than
+         *                  hyperplane described above is considered
+         * @return result of comparision
+         */
         boolean intersects(A area, ArrayList<T> points, ArrayList<Boolean> ascending);
+
+        /**
+         * defines a distance between a and b
+         *
+         * @param a first point to compare
+         * @param b second point to compare
+         * @return result of comparison
+         */
         double distance(T a, T b);
+
+        /**
+         * defines a distance between a and b in given dimension
+         *
+         * @param a         first point to compare
+         * @param b         second point to compare
+         * @param dimension which dimension is considered
+         * @return result of comparison
+         */
         double distance(T a, T b, int dimension);
+
+        /**
+         * defines if a is lower then b in givend dimension
+         *
+         * @param a         first point to compare
+         * @param b         second point to compare
+         * @param dimension which dimension is considered
+         * @return result of comparison
+         */
         boolean lower(T a, T b, int dimension);
     }
+
+    /**
+     * helper interface for simplified tree
+     */
+    public interface Placed {
+        /**
+         * fetches position from class
+         *
+         * @return position
+         */
+        Point2D pos();
+    }
+
+    /**
+     * Comparator for simplified tree
+     */
     public static class CircleComparator implements Contains<Placed, Circle> {
 
         @Override
         public boolean contains(Circle area, Placed point) {
             return area.contains(point.pos());
         }
+
         @Override
         public boolean contains(Circle area, ArrayList<Placed> points) {
             Point2D A = points.get(0).pos(), B = points.get(1).pos(), C = points.get(2).pos(), D = points.get(3).pos();
@@ -116,14 +227,14 @@ public class KdTree<T, A> {
                 double y = points.get(j).pos().getY(), oy = area.getCenterY();
                 if (j % 2 == 0) {
                     double dx = Math.abs(ox - x);
-                    if (dx<=r) {
+                    if (dx <= r) {
                         double dy = Math.sqrt(r * r - dx * dx);
                         candidates.add(new Point2D(x, area.getCenterY() + dy));
                         candidates.add(new Point2D(x, area.getCenterY() - dy));
                     } else if (ascending.get(j) && ox > x || ox < x) candidates.add(new Point2D(ox, oy));
                 } else {
                     double dy = Math.abs(oy - y);
-                    if (dy<=r) {
+                    if (dy <= r) {
                         double dx = Math.sqrt(r * r - dy * dy);
                         candidates.add(new Point2D(area.getCenterX() + dx, y));
                         candidates.add(new Point2D(area.getCenterX() - dx, y));
@@ -144,8 +255,10 @@ public class KdTree<T, A> {
         public double distance(Placed a, Placed b, int dimension) {
             Point2D p1 = a.pos(), p2 = b.pos();
             switch (dimension) {
-                case 0: return Math.abs(p1.getX() - p2.getX());
-                case 1: return Math.abs(p1.getY() - p2.getY());
+                case 0:
+                    return Math.abs(p1.getX() - p2.getX());
+                case 1:
+                    return Math.abs(p1.getY() - p2.getY());
             }
             return -1;
         }
@@ -154,12 +267,18 @@ public class KdTree<T, A> {
         public boolean lower(Placed a, Placed b, int dimension) {
             Point2D p1 = a.pos(), p2 = b.pos();
             switch (dimension) {
-                case 0: return p1.getX() < p2.getX();
-                case 1: return p1.getY() < p2.getY();
+                case 0:
+                    return p1.getX() < p2.getX();
+                case 1:
+                    return p1.getY() < p2.getY();
             }
             return false;
         }
     }
+
+    /**
+     * simplified tree
+     */
     public static class StdKd extends KdTree<Placed, Circle> {
         StdKd(List<Placed> l) throws KdTreeException {
             super(l, new ArrayList<>(planeComparator), new CircleComparator());
@@ -168,20 +287,34 @@ public class KdTree<T, A> {
         private static final List<Comparator<Placed>> planeComparator = Arrays.asList(
                 (Placed o1, Placed o2) -> {
                     int diff = (int) (o1.pos().getX() - o2.pos().getX());
-                    return diff != 0 ? diff : (int)(o1.pos().getY() - o2.pos().getY());
+                    return diff != 0 ? diff : (int) (o1.pos().getY() - o2.pos().getY());
                 },
 
                 (Placed o1, Placed o2) -> {
                     int diff = (int) (o1.pos().getY() - o2.pos().getY());
-                    return diff != 0 ? diff : (int)(o1.pos().getX() - o2.pos().getX());
+                    return diff != 0 ? diff : (int) (o1.pos().getX() - o2.pos().getX());
                 }
         );
     }
 
-    static class Dupa implements Placed { Point2D p; Dupa(double a, double b) { p = new Point2D(a, b);} @Override public Point2D pos() { return p; } static final Dupa ZERO = new Dupa(0,0); }
+    static class Dupa implements Placed {
+        Point2D p;
+
+        Dupa(double a, double b) {
+            p = new Point2D(a, b);
+        }
+
+        @Override
+        public Point2D pos() {
+            return p;
+        }
+
+        static final Dupa ZERO = new Dupa(0, 0);
+    }
+
     public static void main(String[] args) {
         try {
-            StdKd t = new StdKd(Arrays.asList(new Dupa(10,10), new Dupa(-10, -10)));
+            StdKd t = new StdKd(Arrays.asList(new Dupa(10, 10), new Dupa(-10, -10)));
             System.out.println(t.fetchElements(new Circle(14, 14, 30)));
 
             for (int i = 0; i < 100; ++i) {
@@ -194,13 +327,13 @@ public class KdTree<T, A> {
             System.out.println(t.fetchElements(new Circle(14, 14, 30)));
             List<Placed> res = t.fetchElements(new Circle(14, 14, 30));
             t.rmPoint(res.get(1));
-            List<Placed> set = t.fetchElements(new Circle(4,4, 10));
+            List<Placed> set = t.fetchElements(new Circle(4, 4, 10));
             System.out.print("[" + set.size() + ":");
             for (Placed a : set) {
                 System.out.print(a.pos() + ",");
             }
             System.out.println("]");
-            set = t.kNearestNeighbours(6, () -> new Point2D(4,4));
+            set = t.kNearestNeighbours(6, () -> new Point2D(4, 4));
             System.out.print("[" + set.size() + ":");
             for (Placed a : set) {
                 System.out.print(a.pos() + ",");
@@ -211,7 +344,8 @@ public class KdTree<T, A> {
             e.printStackTrace();
         }
     }
-//========================= P R I V A T E ==============================================================================
+
+    //========================= P R I V A T E ==============================================================================
     private Tree<T, A> buildTree(ArrayList<List<T>> sorted, int depth) throws KdTreeException {
         int dimension = depth % sorted.size();
 
@@ -247,10 +381,15 @@ public class KdTree<T, A> {
 
     private interface Tree<T, A> {
         public void getIntersection(A area, List<T> ret);
+
         void pullValues(List<T> ret);
+
         Tree<T, A> addPoint(T point);
+
         Tree<T, A> rmPoint(T point);
+
         void decDepth();
+
         double kNearestNeighbours(T queryPoint, int k, List<T> res);
     }
 
@@ -307,7 +446,7 @@ public class KdTree<T, A> {
         @Override
         public double kNearestNeighbours(T queryPoint, int k, List<T> res) {
             res.add(content);
-            return res.size() >= k ? contains.distance(res.get(k-1), queryPoint) : Double.MAX_VALUE;
+            return res.size() >= k ? contains.distance(res.get(k - 1), queryPoint) : Double.MAX_VALUE;
         }
 
     }
@@ -344,7 +483,7 @@ public class KdTree<T, A> {
 
             if (comparison < 0) {
                 left = left.addPoint(point);
-            } else if (comparison > 0){
+            } else if (comparison > 0) {
                 right = right.addPoint(point);
             }
             return this;
@@ -377,7 +516,7 @@ public class KdTree<T, A> {
 
         @Override
         public double kNearestNeighbours(T queryPoint, int k, List<T> res) {
-            boolean lower = contains.lower(queryPoint, val, depth%c.size());
+            boolean lower = contains.lower(queryPoint, val, depth % c.size());
             Tree<T, A> considered = lower ? left : right;
             double dist = considered.kNearestNeighbours(queryPoint, k, res);
             if (contains.distance(queryPoint, val, depth % c.size()) < dist) {
@@ -397,14 +536,14 @@ public class KdTree<T, A> {
                 ascending.set(part, false);
             }
 
-            if (points.size() == c.size()*2 && contain(area, points, ascending)) {
+            if (points.size() == c.size() * 2 && contain(area, points, ascending)) {
                 left.pullValues(ret);
             } else if (contains.intersects(area, points, ascending)) {
                 left.getIntersection(area, ret);
             }
 
             ascending.set(part, true);
-            if (points.size() == c.size()*2 && contain(area, points, ascending)) {
+            if (points.size() == c.size() * 2 && contain(area, points, ascending)) {
                 right.pullValues(ret);
             } else if (contains.intersects(area, points, ascending)) {
                 right.getIntersection(area, ret);
@@ -419,7 +558,7 @@ public class KdTree<T, A> {
                 }
             }
 
-            return  contains.contains(area, points);
+            return contains.contains(area, points);
         }
     }
 }
