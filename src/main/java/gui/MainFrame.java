24fa8d06514f2a15bd7bfebf5cb40df92eca8d8c
KrzyHonk <honkiszkrzysztof@gmail.com>
2015-02-10 23:44:04 +0100

    Private class for listeners

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -10,6 +10,7 @@ import main.java.utils.AgentInTree;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONWriter;
+import sun.applet.Main;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -114,35 +115,8 @@ public class MainFrame extends JFrame {
             }
 
         });
-
-        this.startSimulationButtonAddActionListener(e -> {
-            if (mouseWheelListener == null) {
-                mouseWheelListener = new BoardMouseWheelListener(this.getBoardPanel());
-                this.getBoardPanel().addMouseWheelListener(mouseWheelListener);
-            } else {
-                mouseWheelListener.simulationStarted = true;
-            }
-
-            motionListener.simulationStarted = true;
-            mouseListener.simulationStarted = true;
-            this.getBoardPanel().simulationStarted = true;
-            this.server.startSimulation();
-        });
-
-        createOpenFileActionListener();
-        createSaveToFileActionListener();
-
-        this.getOptionsPanel().setSidePanelsSliderListener(e -> {
-            JSlider source = (JSlider) e.getSource();
-            SideOptionPanel p = findParent(source, SideOptionPanel.class);
-            p.sliderMoved();
-        });
-
-        this.spawnAgentsAddActionListener((e) -> {
-            Pair<Integer, Integer> size = this.getOptionsPanel().getBoardSize();
-            prepareMouseListenersAndBoard(size.getValue(), size.getKey());
-            this.server.prepareSimulation(this.getOptionsPanel().getBluesAgents(), this.getOptionsPanel().getRedsAgents(), null, this.getOptionsPanel().getTimeStep());
-        });
+        PrepareListeners prepare = new PrepareListeners(this);
+        prepare.PrepareToListen();
     }
 
     public void redrawBoard(List<AgentInTree> agents) {
@@ -185,119 +159,6 @@ public class MainFrame extends JFrame {
         lblMorale.setText("");
     }
 
-    /* Metody pomocnicze dla listenerów */
-    private void prepareMouseListenersAndBoard(int boardWidth, int boardHeight) {
-        if (mouseWheelListener != null) {
-            mouseWheelListener.simulationStarted = false;
-        }
-        if (motionListener == null) {
-            motionListener = new BoardMouseMotionListener(this.getBoardPanel());
-            mouseListener = new BoardMouseListener(this.getBoardPanel());
-            this.getBoardPanel().innerBoard.addMouseMotionListener(motionListener);
-            this.getBoardPanel().innerBoard.addMouseListener(mouseListener);
-        } else {
-            motionListener.simulationStarted = false;
-            mouseListener.simulationStarted = false;
-        }
-        this.getBoardPanel().simulationStarted = false;
-        this.getBoardPanel().generateBoard(boardHeight, boardWidth);
-        this.getBoardPanel().resetScale();
-    }
-
-    private void createOpenFileActionListener() {
-        this.getOptionsPanel().openFileAddActionListener(e -> {
-            int returnVal = this.getOptionsPanel().getFileChooser().showOpenDialog(this);
-
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = this.getOptionsPanel().getFileChooser().getSelectedFile();
-                //This is where a real application would open the file.
-                String content = "";
-                Scanner scanner = null;
-                try {
-                    scanner = new Scanner(new FileInputStream(file));
-                    while (scanner.hasNext())
-                        content += scanner.nextLine();
-
-                    scanner.close();
-                    JSONObject obj = new JSONObject(content);
-                    JSONArray agents = obj.getJSONArray("agents");
-
-                    HashMap<String, ArrayList<JSONObject>> map = new HashMap<>();
-
-                    for (int i = 0; i < agents.length(); i++) {
-                        JSONObject agent = agents.getJSONObject(i);
-
-                        if (map.containsKey(agent.get("type").toString()))
-                            map.get(agent.get("type").toString()).add(agent);
-                        else {
-                            ArrayList<JSONObject> lst = new ArrayList<>();
-                            lst.add(agent);
-                            map.put(agent.get("type").toString(), lst);
-                        }
-                    }
-                    prepareMouseListenersAndBoard(obj.getInt("boardWidth"),obj.getInt("boardHeight"));
-                    //frame.server.prepareSimulation(null,null,map, obj.getInt("boardWidth"),frame.getOptionsPanel().getTimeStep());
-                    this.server.prepareSimulation(null,null,map,this.getOptionsPanel().getTimeStep());
-                    this.getOptionsPanel().setBoardSize(obj.getInt("boardWidth"),obj.getInt("boardHeight"));
-                } catch (FileNotFoundException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void createSaveToFileActionListener() {
-        this.getOptionsPanel().saveToFileAddActionListener(e -> {
-            int returnVal = this.getOptionsPanel().getFileChooser().showSaveDialog(this);
-
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File f = this.getOptionsPanel().getFileChooser().getSelectedFile();
-                try {
-                    FileWriter fw = new FileWriter(f);
-                    Pair<Integer,Integer> size = this.getOptionsPanel().getBoardSize();
-                    JSONWriter w = new JSONWriter(fw);
-                    w.object();
-                    w.key("boardWidth").value(size.getValue());
-                    w.key("boardHeight").value(size.getKey());
-
-                    this.server.updateTree();
-
-                    double[] testKey = {0,0};
-                    double[] upperKey = {size.getValue()*this.getBoardPanel().SQUARESIZE,size.getKey()*this.getBoardPanel().SQUARESIZE};
-                    try {
-                        java.util.List<AgentInTree> lst = this.server.getWorld().getAgentsTree().range(testKey, upperKey);
-                        w.key("agents");
-                        w.array();
-                        for (AgentInTree a : lst) {
-                            w.object();
-                            w.key("x").value(a.p.getX());
-                            w.key("y").value(a.p.getY());
-                            w.key("type").value(a.type);
-                            w.key("side").value(a.side);
-                            if (a.behaviourClass != null) {
-                                String b = a.behaviourClass.getName();
-                                w.key("behaviour").value(b.substring(b.lastIndexOf('.')+1));
-                            }
-                            else
-                                w.key("behaviour").value("");
-                            w.endObject();
-                        }
-                        w.endArray();
-                        //w.endObject();
-                    } catch (KeySizeException e1) {
-                        e1.printStackTrace();
-                    }
-                    w.endObject();
-                    fw.flush();
-                    fw.close();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-
-            }
-        });
-    }
-
     public static <T extends Container> T findParent(Component comp, Class<T> clazz)  {
         if (comp == null)
             return null;
@@ -311,7 +172,6 @@ public class MainFrame extends JFrame {
     public BoardPanel getBoardPanel() {
         return boardPanel;
     }
-
     public OptionsPanel getOptionsPanel() {
         return optionsPanel;
     }
@@ -319,8 +179,168 @@ public class MainFrame extends JFrame {
     public double getFRAME_WIDTH() {
         return FRAME_WIDTH;
     }
-
     public double getFRAME_HEIGHT() {
         return FRAME_HEIGHT;
     }
+
+    /* Prywatna klasa do przygotowania listenerów */
+    private class PrepareListeners {
+        private MainFrame frame;
+        public PrepareListeners(MainFrame frame){
+            this.frame = frame;
+        }
+
+        public void PrepareToListen() {
+            createOpenFileActionListener();
+            createSaveToFileActionListener();
+
+            this.startSimulationButtonAddActionListener(e -> {
+                if (frame.mouseWheelListener == null) {
+                    mouseWheelListener = new BoardMouseWheelListener(frame.getBoardPanel());
+                    frame.getBoardPanel().addMouseWheelListener(mouseWheelListener);
+                } else {
+                    mouseWheelListener.simulationStarted = true;
+                }
+
+                motionListener.simulationStarted = true;
+                mouseListener.simulationStarted = true;
+                frame.getBoardPanel().simulationStarted = true;
+                frame.server.startSimulation();
+            });
+
+            frame.getOptionsPanel().setSidePanelsSliderListener(e -> {
+                JSlider source = (JSlider) e.getSource();
+                SideOptionPanel p = findParent(source, SideOptionPanel.class);
+                p.sliderMoved();
+            });
+
+            frame.spawnAgentsAddActionListener((e) -> {
+                Pair<Integer, Integer> size = frame.getOptionsPanel().getBoardSize();
+                prepareMouseListenersAndBoard(size.getValue(), size.getKey());
+                frame.server.prepareSimulation(frame.getOptionsPanel().getBluesAgents(), frame.getOptionsPanel().getRedsAgents(), null, frame.getOptionsPanel().getTimeStep());
+            });
+        }
+
+        public void spawnAgentsAddActionListener(ActionListener listener) {
+            spawnAgents.addActionListener(listener);
+        }
+
+        public void startSimulationButtonAddActionListener(ActionListener listener) {
+            btnStartSimulation.addActionListener(listener);
+        }
+
+        private void prepareMouseListenersAndBoard(int boardWidth, int boardHeight) {
+            if (mouseWheelListener != null) {
+                mouseWheelListener.simulationStarted = false;
+            }
+            if (motionListener == null) {
+                motionListener = new BoardMouseMotionListener(frame.getBoardPanel());
+                mouseListener = new BoardMouseListener(frame.getBoardPanel());
+                frame.getBoardPanel().innerBoard.addMouseMotionListener(motionListener);
+                frame.getBoardPanel().innerBoard.addMouseListener(mouseListener);
+            } else {
+                motionListener.simulationStarted = false;
+                mouseListener.simulationStarted = false;
+            }
+            frame.getBoardPanel().simulationStarted = false;
+            frame.getBoardPanel().generateBoard(boardHeight, boardWidth);
+            frame.getBoardPanel().resetScale();
+        }
+
+        private void createOpenFileActionListener() {
+            frame.getOptionsPanel().openFileAddActionListener(e -> {
+                int returnVal = frame.getOptionsPanel().getFileChooser().showOpenDialog(frame);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = frame.getOptionsPanel().getFileChooser().getSelectedFile();
+                    //This is where a real application would open the file.
+                    String content = "";
+                    Scanner scanner = null;
+                    try {
+                        scanner = new Scanner(new FileInputStream(file));
+                        while (scanner.hasNext())
+                            content += scanner.nextLine();
+
+                        scanner.close();
+                        JSONObject obj = new JSONObject(content);
+                        JSONArray agents = obj.getJSONArray("agents");
+
+                        HashMap<String, ArrayList<JSONObject>> map = new HashMap<>();
+
+                        for (int i = 0; i < agents.length(); i++) {
+                            JSONObject agent = agents.getJSONObject(i);
+
+                            if (map.containsKey(agent.get("type").toString()))
+                                map.get(agent.get("type").toString()).add(agent);
+                            else {
+                                ArrayList<JSONObject> lst = new ArrayList<>();
+                                lst.add(agent);
+                                map.put(agent.get("type").toString(), lst);
+                            }
+                        }
+                        prepareMouseListenersAndBoard(obj.getInt("boardWidth"),obj.getInt("boardHeight"));
+                        //frame.server.prepareSimulation(null,null,map, obj.getInt("boardWidth"),frame.getOptionsPanel().getTimeStep());
+                        frame.server.prepareSimulation(null,null,map,frame.getOptionsPanel().getTimeStep());
+                        frame.getOptionsPanel().setBoardSize(obj.getInt("boardWidth"),obj.getInt("boardHeight"));
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        private void createSaveToFileActionListener() {
+            frame.getOptionsPanel().saveToFileAddActionListener(e -> {
+                int returnVal = frame.getOptionsPanel().getFileChooser().showSaveDialog(frame);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File f = frame.getOptionsPanel().getFileChooser().getSelectedFile();
+                    try {
+                        FileWriter fw = new FileWriter(f);
+                        Pair<Integer,Integer> size = frame.getOptionsPanel().getBoardSize();
+                        JSONWriter w = new JSONWriter(fw);
+                        w.object();
+                        w.key("boardWidth").value(size.getValue());
+                        w.key("boardHeight").value(size.getKey());
+
+                        frame.server.updateTree();
+
+                        double[] testKey = {0,0};
+                        double[] upperKey = {size.getValue()*frame.getBoardPanel().SQUARESIZE,size.getKey()*frame.getBoardPanel().SQUARESIZE};
+                        try {
+                            java.util.List<AgentInTree> lst = frame.server.getWorld().getAgentsTree().range(testKey, upperKey);
+                            w.key("agents");
+                            w.array();
+                            for (AgentInTree a : lst) {
+                                w.object();
+                                w.key("x").value(a.p.getX());
+                                w.key("y").value(a.p.getY());
+                                w.key("type").value(a.type);
+                                w.key("side").value(a.side);
+                                if (a.behaviourClass != null) {
+                                    String b = a.behaviourClass.getName();
+                                    w.key("behaviour").value(b.substring(b.lastIndexOf('.')+1));
+                                }
+                                else
+                                    w.key("behaviour").value("");
+                                w.endObject();
+                            }
+                            w.endArray();
+                            //w.endObject();
+                        } catch (KeySizeException e1) {
+                            e1.printStackTrace();
+                        }
+                        w.endObject();
+                        fw.flush();
+                        fw.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+
+                }
+            });
+        }
+    }
+
 }
+
